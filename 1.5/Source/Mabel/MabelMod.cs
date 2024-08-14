6023cc154ef4d0a1b88e21186e5c7f2aff60dc60
Dexter Lowe <feldoh10@gmail.com>
2024-08-14 20:47:35 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/Mabel/MabelMod.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Mabel;
+
+public class MabelMod : Mod
+{
+    public static Settings settings;
+
+    public MabelMod(ModContentPack content) : base(content)
+    {
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("Taggerung.rimworld.Mabel.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Mabel_SettingsCategory".Translate();
+    }
+}
